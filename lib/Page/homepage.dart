f4c94b1cdcd581cfbafe3523fac61cf677086546
kcuyh <khj879123@gmail.com>
2019-11-27 18:49:14 +0900

    0.0.1

--- lib/Page/homepage.dart
+++ lib/Page/homepage.dart
@@ -35,15 +35,53 @@ class _HomePageState extends State<HomePage> {
                   ],
                 ),//추천
                 SizedBox(height: 30,),
-                Container(
-                  width: MediaQuery.of(context).size.width /1.2,
-                  height: MediaQuery.of(context).size.height/25,
-                  decoration: BoxDecoration(
-                  ),
-                ),
+                Recommend(context, '필요한 토이 프로젝트를 찾는법'),
+                Recommend(context, '뗄감이 되지않는 3가지 방법'),
+                Recommend(context, '좋은 회사를 찾는 노하우'),
+                SizedBox(height: 20,),
+                Row(
+                  children: <Widget>[
+                    SizedBox(width: 30,),
+                    Container(
+                      child: Text(
+                        '메뉴',
+                        style: TextStyle(
+                          fontSize: MediaQuery.of(context).textScaleFactor*30,
+                          color: Colors.white,
+                          fontFamily: 'RIDI',
+                        ),
+                      ),
+                    ),
+                  ],
+                ),//메뉴
               ],
             ),
           ],
+        ),
+      ),
+    );
+  }
+
+  Widget Recommend(BuildContext context, String content){
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        width: MediaQuery.of(context).size.width /1.2,
+        height: MediaQuery.of(context).size.height/15,
+        decoration: BoxDecoration(
+          border: Border.all(width: 0.3, color: Colors.white70),
+          borderRadius: BorderRadius.all(Radius.circular(10)),
+          color: Colors.black38,
+        ),
+        child: Center(
+          child: Text(
+            content,
+            style: TextStyle(
+                fontSize: MediaQuery.of(context).textScaleFactor*20,
+                fontFamily: 'RIDI',
+                color: Colors.white
+            ),
+          ),
         ),
       ),
     );
